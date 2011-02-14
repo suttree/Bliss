1ada4fc18b3d859279835447ac721e391e0b465a
Duncan Gough <duncan@suttree.com>
2011-02-14 22:56:48 +0000

    Attract mode - letting the game play itself and watching how the whole second-screen, passive version of the comings and goings of people checking in to Bliss feels

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var pidfile = fs.openSync("/var/tmp/node-" + node_env + ".pid", "w");
 fs.writeSync(pidfile, process.pid + "");
 fs.closeSync(pidfile);
 
-//var demo = require(__dirname + '/lib/demo');
 var nlog = require(__dirname + '/lib/logging');
+//var demo = require(__dirname + '/lib/demo');
 var nearest = require(__dirname + '/lib/nearest');
 
 function log(message) {
@@ -120,70 +120,6 @@ function current_status(id, type, player) {
 var players = {
   labels: []
 };
-
-// A fake player at Dartford station
-players.labels.push(4);
-players[4] = {
-  id: 4,
-  lat: 51.44737,
-  lon: 0.21926
-};
-
-// A fake player at Angel station
-players.labels.push(5);
-players[5] = {
-  id: 5,
-  lat: 51.5324989, 
-  lon: -0.1057899
-};
-
-// Another fake player in The Island Queen, Islington
-players.labels.push(6);
-players[6] = {
-  id: 6,
-  lat: 51.53313578916992,
-  lon: -0.09823322296142578
-};
-
-// And another player at the Screen on the Green, Islington
-players.labels.push(7);
-players[7] = {
-  id: 7,
-  lat: 51.53649936111712,
-  lon: -0.10364055633544922
-};
-
-// Here's a player in Thirza Road, Dartford
-players.labels.push(8);
-players[8] = {
-  id: 8,
-  lat: 51.443923918816395, 
-  lon: 0.2287602424621582
-}
-
-// Here's a player in Kenwyn Road, Dartford
-players.labels.push(9);
-players[9] = {
-  id: 9,
-  lat: 51.45162677603857, 
-  lon: 0.21438360214233398
-}
-
-// And one more in the M&S in Dartford
-players.labels.push(10);
-players[10] = {
-  id: 10,
-  lat: 51.44794935873729, 
-  lon: 0.21680831909179688
-}
-
-// And here's Mark Dagget snowed in, in New York City
-players.labels.push(11);
-players[11] = {
-  id: 11,
-  lat: -73.964003,
-  lon: 40.773503
-}
 
 server = http.createServer(function(req, res){ 
   res.writeHead(200, {'Content-Type': 'text/html'}); 
